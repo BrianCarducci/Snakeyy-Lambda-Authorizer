d67f8d135f8edd8c7d7cf586e1df1baadfb09abc
BrianCarducci <briancarducci@gmail.com>
2020-05-07 00:57:24 -0400

    filled in values for token req

--- hello/main.go
+++ hello/main.go
@@ -5,6 +5,7 @@ import (
 	"io/ioutil"
 	"net/http"
 	"net/url"
+	"os"
 
 	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
@@ -21,45 +22,53 @@ func Handler(request events.APIGatewayProxyRequest) (events.APIGatewayProxyRespo
 
 	code := request.PathParameters["code"]
 	fmt.Println("code: " + code)
+
+	if len(code) == 0 {
+		fmt.Println("No authorization code supplied")
+		return events.APIGatewayProxyResponse{
+			StatusCode: 400,
+			Body: "No authorization code supplied",
+		}, nil
+	}
 	
 	values := url.Values{}
 	values.Add("code", code)
 	values.Add("CLIENT_ID", "270736967654851")
-	values.Add("CLIENT_SECRET", "")
+	values.Add("CLIENT_SECRET", os.Getenv("SNAKEYY_CLIENT_SECRET"))
 	values.Add("grant_type", "authorization_code")
-	values.Add("redirect_uri", "")
+	values.Add("redirect_uri", "https://2afo5m8bll.execute-api.us-east-1.amazonaws.com/dev/hello")
 	
 	tokenReq, err := http.NewRequest("POST", "https://instagram.com/oauth/access_token?" + values.Encode(), nil)
 	if err != nil {
-		fmt.Println("Failed to assemble token request")
-		return response{
+		fmt.Println("Failed to assemble token request: " + err.Error())
+		return events.APIGatewayProxyResponse{
 			StatusCode: 500,
-			Body: "Failed to assemble request"
-		}
+			Body: "Failed to assemble request: " + err.Error(),
+		}, nil
 	}
 
 	tokenRes, err := http.DefaultClient.Do(tokenReq)
 	if err != nil {
-		fmt.Println("Error occurred in making token request")
-		return response{
+		fmt.Println("Error occurred in making token request: " + err.Error())
+		return events.APIGatewayProxyResponse{
 			StatusCode: 500,
-			Body: "Error occurred in making token request"
-		}
+			Body: "Error occurred in making token request: " + err.Error(),
+		}, nil
 	}
 
 	tokenResBody, err := ioutil.ReadAll(tokenRes.Body)
 	if err != nil {
-		fmt.Println("Failed to read token response body")
-		return response{
+		fmt.Println("Failed to read token response body: " + err.Error())
+		return events.APIGatewayProxyResponse{
 			StatusCode: 500,
-			Body: "Failed to read token response body"
-		}
+			Body: "Failed to read token response body: " + err.Error(),
+		}, nil
 	}
 
-	return response{
+	return events.APIGatewayProxyResponse{
 		StatusCode: 200,
-		Body: string(tokenResBody)
-	}
+		Body: string(tokenResBody),
+	}, nil
 
 	// return events.APIGatewayProxyResponse{Body: request.Body, StatusCode: 200}, nil
 }
