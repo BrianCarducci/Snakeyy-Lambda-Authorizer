d100ab31d71b547fe49e81e9e5802c49475a1add
BrianCarducci <briancarducci@gmail.com>
2020-05-07 21:55:07 -0400

    getting closer

--- hello/main.go
+++ hello/main.go
@@ -6,6 +6,7 @@ import (
 	"net/http"
 	"net/url"
 	"os"
+	"strings"
 
 	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
@@ -20,10 +21,10 @@ var response events.APIGatewayProxyResponse
 func Handler(request events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
 	fmt.Println("Received body: ", request.Body)
 
-	code := request.PathParameters["code"]
+	code := request.QueryStringParameters["code"]
 	fmt.Println("code: " + code)
 
-	if len(code) == 0 {
+	if code == "" {
 		fmt.Println("No authorization code supplied")
 		return events.APIGatewayProxyResponse{
 			StatusCode: 400,
@@ -31,14 +32,14 @@ func Handler(request events.APIGatewayProxyRequest) (events.APIGatewayProxyRespo
 		}, nil
 	}
 	
-	values := url.Values{}
-	values.Add("code", code)
-	values.Add("CLIENT_ID", "270736967654851")
-	values.Add("CLIENT_SECRET", os.Getenv("SNAKEYY_CLIENT_SECRET"))
-	values.Add("grant_type", "authorization_code")
-	values.Add("redirect_uri", "https://2afo5m8bll.execute-api.us-east-1.amazonaws.com/dev/hello")
+	form := url.Values{}
+	form.Add("code", code)
+	form.Add("client_id", "270736967654851")
+	form.Add("client_secret", os.Getenv("SNAKEYY_CLIENT_SECRET"))
+	form.Add("grant_type", "authorization_code")
+	form.Add("redirect_uri", "https://2afo5m8bll.execute-api.us-east-1.amazonaws.com/dev/hello/")
 	
-	tokenReq, err := http.NewRequest("POST", "https://instagram.com/oauth/access_token?" + values.Encode(), nil)
+	tokenReq, err := http.NewRequest("POST", "https://api.instagram.com/oauth/access_token", strings.NewReader(form.Encode()))
 	if err != nil {
 		fmt.Println("Failed to assemble token request: " + err.Error())
 		return events.APIGatewayProxyResponse{
@@ -46,6 +47,7 @@ func Handler(request events.APIGatewayProxyRequest) (events.APIGatewayProxyRespo
 			Body: "Failed to assemble request: " + err.Error(),
 		}, nil
 	}
+	tokenReq.Header.Add("Content-Type", "application/x-www-form-urlencoded")
 
 	tokenRes, err := http.DefaultClient.Do(tokenReq)
 	if err != nil {
@@ -76,32 +78,3 @@ func Handler(request events.APIGatewayProxyRequest) (events.APIGatewayProxyRespo
 func main() {
 	lambda.Start(Handler)
 }
-
-// Handler is our lambda handler invoked by the `lambda.Start` function call
-// func Handler(ctx context.Context) (Response, error) {
-// 	var buf bytes.Buffer
-
-// 	body, err := json.Marshal(map[string]interface{}{
-// 		"message": "Go Serverless v1.0! Your function executed successfully!",
-// 	})
-// 	if err != nil {
-// 		return Response{StatusCode: 404}, err
-// 	}
-// 	json.HTMLEscape(&buf, body)
-
-// 	resp := Response{
-// 		StatusCode:      200,
-// 		IsBase64Encoded: false,
-// 		Body:            buf.String(),
-// 		Headers: map[string]string{
-// 			"Content-Type":           "application/json",
-// 			"X-MyCompany-Func-Reply": "hello-handler",
-// 		},
-// 	}
-
-// 	return resp, nil
-// }
-
-// func main() {
-// 	lambda.Start(Handler)
-// }
