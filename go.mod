a2759c12f22ecc1ba7a271c3ff9a61ea5ad9a70b
BrianCarducci <briancarducci@gmail.com>
2020-05-07 23:04:43 -0400

    now successfully retrieves access token

--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module github.com/BrianCarducci/Snakeyy-Lambda-Authorizer
 
 go 1.14
 
-require github.com/aws/aws-lambda-go v1.16.0 // indirect
+require (
+	github.com/aws/aws-lambda-go v1.16.0 // indirect
+	github.com/aws/aws-sdk-go v1.30.23 // indirect
+)
--- hello/main.go
+++ hello/main.go
@@ -1,15 +1,20 @@
 package main
 
 import (
+	"encoding/json"
+	"errors"
 	"fmt"
 	"io/ioutil"
 	"net/http"
 	"net/url"
-	"os"
 	"strings"
 
 	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/awserr"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/secretsmanager"
 )
 
 // Response is of type APIGatewayProxyResponse since we're leveraging the
@@ -31,11 +36,20 @@ func Handler(request events.APIGatewayProxyRequest) (events.APIGatewayProxyRespo
 			Body: "No authorization code supplied",
 		}, nil
 	}
-	
+
+	secret, err := getSecretByName("SNAKEYY_CLIENT_SECRET")
+	if err != nil {
+		fmt.Println("Failed to get secret: " + err.Error())
+		return events.APIGatewayProxyResponse{
+			StatusCode: 400,
+			Body: "Failed to get secret: " + err.Error(),
+		}, nil
+	}
+
 	form := url.Values{}
 	form.Add("code", code)
 	form.Add("client_id", "270736967654851")
-	form.Add("client_secret", os.Getenv("SNAKEYY_CLIENT_SECRET"))
+	form.Add("client_secret", secret)
 	form.Add("grant_type", "authorization_code")
 	form.Add("redirect_uri", "https://2afo5m8bll.execute-api.us-east-1.amazonaws.com/dev/hello/")
 	
@@ -71,10 +85,69 @@ func Handler(request events.APIGatewayProxyRequest) (events.APIGatewayProxyRespo
 		StatusCode: 200,
 		Body: string(tokenResBody),
 	}, nil
-
-	// return events.APIGatewayProxyResponse{Body: request.Body, StatusCode: 200}, nil
 }
 
 func main() {
 	lambda.Start(Handler)
+}
+
+func getSecretByName(secretName string) (string, error) {
+	region := "us-east-1"
+
+	//Create a Secrets Manager client
+	svc := secretsmanager.New(session.New(),
+                                  aws.NewConfig().WithRegion(region))
+	input := &secretsmanager.GetSecretValueInput{
+		SecretId:     aws.String(secretName),
+		VersionStage: aws.String("AWSCURRENT"), // VersionStage defaults to AWSCURRENT if unspecified
+	}
+
+	// In this sample we only handle the specific exceptions for the 'GetSecretValue' API.
+	// See https://docs.aws.amazon.com/secretsmanager/latest/apireference/API_GetSecretValue.html
+
+	result, err := svc.GetSecretValue(input)
+	if err != nil {
+		if aerr, ok := err.(awserr.Error); ok {
+			switch aerr.Code() {
+				case secretsmanager.ErrCodeDecryptionFailure:
+				// Secrets Manager can't decrypt the protected secret text using the provided KMS key.
+				fmt.Println(secretsmanager.ErrCodeDecryptionFailure, aerr.Error())
+
+				case secretsmanager.ErrCodeInternalServiceError:
+				// An error occurred on the server side.
+				fmt.Println(secretsmanager.ErrCodeInternalServiceError, aerr.Error())
+
+				case secretsmanager.ErrCodeInvalidParameterException:
+				// You provided an invalid value for a parameter.
+				fmt.Println(secretsmanager.ErrCodeInvalidParameterException, aerr.Error())
+
+				case secretsmanager.ErrCodeInvalidRequestException:
+				// You provided a parameter value that is not valid for the current state of the resource.
+				fmt.Println(secretsmanager.ErrCodeInvalidRequestException, aerr.Error())
+
+				case secretsmanager.ErrCodeResourceNotFoundException:
+				// We can't find the resource that you asked for.
+				fmt.Println(secretsmanager.ErrCodeResourceNotFoundException, aerr.Error())
+			}
+		} else {
+			// Print the error, cast err to awserr.Error to get the Code and
+			// Message from an error.
+			fmt.Println(err.Error())
+		}
+		return "", err
+	}
+
+	var secretStringifiedMap string
+	if result.SecretString != nil {
+		secretStringifiedMap = *result.SecretString
+	} else {
+		return "", errors.New("The secret value is nil")
+	}
+	
+	secretMap := map[string]string{}
+	if err := json.Unmarshal([]byte(secretStringifiedMap), &secretMap); err != nil {
+		return "", err
+	}
+
+	return secretMap[secretName], nil
 }
